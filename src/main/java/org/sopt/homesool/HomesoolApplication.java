a23061e9a1a1cd8f07734fbe5273d77043e70c92
euije <exitfill1234@naver.com>
2023-05-14 18:45:09 +0900

    [Feat] #1 - Spring Boot 초기 설정

---
+++ src/main/java/org/sopt/homesool/HomesoolApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.homesool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesoolApplication.class, args);
+	}
+
+}
