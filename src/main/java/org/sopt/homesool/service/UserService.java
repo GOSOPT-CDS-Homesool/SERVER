25218068af1dd371cc4b7fb898d113b98f0cace8
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-05-20 03:44:20 +0900

    [FEAT] #28 - 유저 테이블의 관심상품을 술 테이블의 좋아요 개수로 반환

--- src/main/java/org/sopt/homesool/service/UserService.java
+++ src/main/java/org/sopt/homesool/service/UserService.java
@@ -2,28 +2,38 @@ package org.sopt.homesool.service;
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.homesool.controller.dto.response.UserResponseDto;
+import org.sopt.homesool.domain.Alcohol;
 import org.sopt.homesool.domain.User;
+import org.sopt.homesool.infrastructure.AlcoholRepository;
 import org.sopt.homesool.infrastructure.UserRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final AlcoholRepository alcoholRepository;
 
     @Transactional
     public UserResponseDto findUserById(Long id) {
         User user = userRepository.findById(id).orElseThrow(()-> new IllegalArgumentException());
+        long likeCount = alcoholRepository.findAll()
+                .stream()
+                .map(alcohol -> alcohol.isLike())
+                .filter(like -> like == true)
+                .count();
+
         return UserResponseDto.builder()
                 .id(id)
                 .nickName(user.getNickName())
                 .userRank(user.getUserRank())
                 .point(user.getPoint())
                 .coupon(user.getCoupon())
-                .interest(user.getInterest())
+                .interest(likeCount)
                 .waiting(user.getWaiting())
                 .finish(user.getFinish())
                 .ready(user.getReady())
