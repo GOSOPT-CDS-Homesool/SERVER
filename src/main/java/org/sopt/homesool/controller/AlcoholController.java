da2063648c392be3b5d039c38a75f13347760008
euije <exitfill1234@naver.com>
2023-05-19 18:25:47 +0900

    [FEAT] #16, #18 - Controller 추가

--- src/main/java/org/sopt/homesool/controller/AlcoholController.java
+++ src/main/java/org/sopt/homesool/controller/AlcoholController.java
@@ -3,7 +3,9 @@ package org.sopt.homesool.controller;
 import lombok.RequiredArgsConstructor;
 import org.sopt.homesool.common.dto.ApiResponse;
 import org.sopt.homesool.common.dto.SuccessStatus;
+import org.sopt.homesool.controller.dto.response.AlcoholDetailResponseDto;
 import org.sopt.homesool.controller.dto.response.AlcoholResponseDto;
+import org.sopt.homesool.controller.dto.response.ProductInquiryResponseDto;
 import org.sopt.homesool.controller.dto.response.ReviewResponseDto;
 import org.sopt.homesool.infrastructure.AlcoholRepository;
 import org.sopt.homesool.service.AlcoholService;
@@ -29,7 +31,19 @@ public class AlcoholController {
 
     @GetMapping("/review/{alcoholId}")
     @ResponseStatus(HttpStatus.OK)
-    public  ApiResponse<List<ReviewResponseDto>> reviewRead(@PathVariable Long alcoholId) {
+    public ApiResponse<List<ReviewResponseDto>> reviewRead(@PathVariable("alcoholId") Long alcoholId) {
         return ApiResponse.success(SuccessStatus.ALCOHOL_REVIEW_FIND_SUCCESS, reviewService.findByAlcoholId(alcoholId));
+    }
+
+    @GetMapping("/{alcoholId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<AlcoholDetailResponseDto> readAlcoholDetail(@PathVariable("alcoholId") Long alcoholId) {
+        return ApiResponse.success(SuccessStatus.ALCOHOL_DETAIL_INFO_SUCCESS, alcoholService.findAlcoholDetail(alcoholId));
+    }
+
+    @GetMapping("/inquiry/{alcoholId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<List<ProductInquiryResponseDto>> readAlcoholInquiry(@PathVariable("alcoholId") Long alcoholId) {
+        return ApiResponse.success(SuccessStatus.ALCOHOL_FIND_INQUIRY_SUCCESS, alcoholService.findAlcoholInquiry(alcoholId));
     }
 }
