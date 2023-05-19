4d882c4a540b1a4829f58d8fd891303a5d67b8d4
euije <exitfill1234@naver.com>
2023-05-19 22:49:22 +0900

    [FEAT] #24 - 결제 성공 메시지 구현

--- src/main/java/org/sopt/homesool/common/dto/SuccessStatus.java
+++ src/main/java/org/sopt/homesool/common/dto/SuccessStatus.java
@@ -21,7 +21,11 @@ public enum SuccessStatus {
     ALCOHOL_FIND_SUCCESS(HttpStatus.OK, "술 조회에 성공하였습니다."),
     ALCOHOL_REVIEW_FIND_SUCCESS(HttpStatus.OK, "술 리뷰 조회에 성공하였습니다."),
     ALCOHOL_DETAIL_INFO_SUCCESS(HttpStatus.OK, "술 상세 정보 조회에 성공하였습니다."),
-    ALCOHOL_FIND_INQUIRY_SUCCESS(HttpStatus.OK, "술 상품 문의 조회에 성공하였습니다.")
+    ALCOHOL_FIND_INQUIRY_SUCCESS(HttpStatus.OK, "술 상품 문의 조회에 성공하였습니다."),
+    /*
+    payment
+     */
+    PAYMENT_SUCCESS(HttpStatus.CREATED, "결제가 성공적으로 완료되었습니다.")
     ;
 
     private final HttpStatus httpStatus;
